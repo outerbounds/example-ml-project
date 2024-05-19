e174ba46f5efb4595f438a989e387616cfbc59ec
root <tuulos@gmail.com>
2024-05-19 04:20:19 +0000

    add hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+from metaflow import FlowSpec, step, card, schedule
+
+class HelloFlow(FlowSpec):
+
+    @step
+    def start(self):
+        self.x = 123
+        print("Hello world!")
+        self.next(self.end)
+
+    @step
+    def end(self):
+        print(self.x)
+
+if __name__ == "__main__":
+    HelloFlow()
