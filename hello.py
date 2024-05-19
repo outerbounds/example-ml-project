256f0b418e9e6394befd9c49f84a4e0f39563bbc
root <tuulos@gmail.com>
2024-05-19 06:21:35 +0000

    add cucumber

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-from metaflow import FlowSpec, step, card, project
+from metaflow import FlowSpec, step, card, project, current
 
 @project(name='mlproject')
 class GitHubActionsDemo(FlowSpec):
 
     @step
     def start(self):
+        self.x = 'cucumber'
+        print('x is', self.x)
+        print('branch is', current.branch_name)
         print("Hello GitHub Actions!")
         self.next(self.end)
 
