de3747011ce7c5b5e84ce414ae45476587abcd74
root <tuulos@gmail.com>
2024-09-08 22:59:46 +0000

    better version

---
+++ loraflow.py
@@ -0,0 +1,20 @@
+from metaflow import FlowSpec, step, card, project, current
+
+@project(name='mlproject')
+class GitHubActionsDemo(FlowSpec):
+
+    @step
+    def start(self):
+        self.x = 'cucumber'
+        print('x is', self.x)
+        print('branch is', current.branch_name)
+        print("Hello GitHub Actions!")
+        self.next(self.end)
+
+    @step
+    def end(self):
+        self.model_accuracy = 0.91
+        print('fixed')
+
+if __name__ == "__main__":
+    GitHubActionsDemo()
