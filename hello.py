93da28f9409ee00b7d797b0940d78865083e2b80
root <tuulos@gmail.com>
2024-11-04 20:48:54 +0000

    better model

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from metaflow import FlowSpec, step, card, project
 
 @project(name='mlproject')
-class GitHubActionsDemo(FlowSpec):
+class GitHubActionsDemo2(FlowSpec):
 
     @step
     def start(self):
@@ -13,4 +13,4 @@ class GitHubActionsDemo(FlowSpec):
         print('fixed')
 
 if __name__ == "__main__":
-    GitHubActionsDemo()
+    GitHubActionsDemo2()
